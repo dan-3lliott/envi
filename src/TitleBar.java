8d993daf7ffad5e692841174865eba3a6b9544cd
dan-3lliott <elliott.daniel.ballantyne@gmail.com>
2019-12-31 14:08:49 -0700

    Styling Components

--- src/TitleBar.java
+++ src/TitleBar.java
@@ -4,8 +4,8 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
-
 import java.util.Arrays;
 
 public class TitleBar extends HBox {
@@ -20,23 +20,26 @@ public class TitleBar extends HBox {
         setSpacing(5);
         setPadding(new Insets(10));
         //create buttons
-        ComboBox changeStyle = new ComboBox(FXCollections.observableList(Arrays.asList(Main.styles)));
+        ComboBox style = new ComboBox(FXCollections.observableList(Arrays.asList(Main.styles)));
         Button minimize = new Button("—");
         Button close = new Button(" X ");
-        //define action for the change style combobox
-        changeStyle.setOnAction(e -> {
+        //define action for the style combobox
+        style.setOnAction(e -> {
             ((ComboBox)e.getSource()).getScene().getStylesheets().clear();
-            ((ComboBox)e.getSource()).getScene().getStylesheets().add(changeStyle.getValue().toString());
+            ((ComboBox)e.getSource()).getScene().getStylesheets().add(style.getValue().toString());
         });
         //define action for the minimize button
         minimize.setOnAction(e -> ((Stage)((Button)e.getSource()).getScene().getWindow()).setIconified(true));
         //define action for the close button
         close.setOnAction(e -> ((Stage)((Button)e.getSource()).getScene().getWindow()).close());
         //remove borders while leaving the rest of the css styling for buttons intact
-        changeStyle.setStyle(noBorder);
+        style.setStyle(noBorder);
         minimize.setStyle(noBorder);
         close.setStyle(noBorder);
+        //ensure that minimize and close buttons don't have their text reduced to ...
+        minimize.setMinWidth(35);
+        close.setMinWidth(35);
         //add buttons to hbox
-        getChildren().addAll(changeStyle, minimize, close);
+        getChildren().addAll(style, minimize, close);
     }
 }
